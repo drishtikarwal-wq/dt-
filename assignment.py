d28817c38654686f8fd3692db24a1a7b0a059abb
drishtikarwal-wq <drishti.karwal@student.wegc.school.nz>
2026-05-14 14:49:52 +1200

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,45 @@
+##
+#social_tracker.py
+#tracks your social activity
+#drishti
+#17
+#11.05.26
+
+def all_posts():
+    """shows post title and content type"""
+    print("\n--- All Posts ---")
+    # loop over each post that is stored in the program
+    for item in items:
+        
+    
+
+#while loop to repeat menu and functions
+again = True
+
+while again:
+
+    print("Menu: ")
+    print("1. _all_posts")
+    print("2. Add_a_post")
+    print("3 Change_number_of_views")
+    print("4. Delete_a_post")
+    print("5. generate_a_post")
+    print("6. Exit")
+
+    choice = input("enter your choice")
+
+    if choice == "1":
+        
+        
+        
+
+
+
+
+
+
+
+    
+    
+    
+
